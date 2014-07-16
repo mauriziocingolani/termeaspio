d6be1deec1c25d7d0910581ea1156e720183abf7
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2014-07-16 15:59:40 +0200

    Home page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -7,9 +7,44 @@
 <div class="row">
     <div class="col-sm-12">
 
-    <h3>termeaspio.it</h3>
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators" style="color: black;">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+            </ol>
 
-    <h1>UNDER CONSTRUCTION</h1>
-    
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img src="/images/home/home1.jpg" alt="home1">
+                    <div class="carousel-caption">
+                        ...
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="/images/home/home2.jpg" alt="home2">
+                    <div class="carousel-caption">
+                        ...
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="/images/home/home3.jpg" alt="home3">
+                    <div class="carousel-caption">
+                        ...
+                    </div>
+                </div>
+            </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+        </div>
+
     </div>
 </div>
